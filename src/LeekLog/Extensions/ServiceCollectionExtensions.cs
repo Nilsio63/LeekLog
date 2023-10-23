b18099d4a6b5892efa30aacd337800ae43ff4ce5
Nils Balke <nilsbalke@t-online.de>
2023-10-23 22:43:00 +0200

    outsourced service registrations

---
+++ src/LeekLog/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using LeekLog.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace LeekLog.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUserAuthService, UserAuthService>();
+
+        services.AddScoped<LeekLogAuthenticationStateProvider>();
+        services.AddScoped<ILeekLogAuthenticationStateProvider>(s => s.GetRequiredService<LeekLogAuthenticationStateProvider>());
+        services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<LeekLogAuthenticationStateProvider>());
+
+        return services;
+    }
+}
--- src/LeekLog/Program.cs
+++ src/LeekLog/Program.cs
@@ -1,8 +1,7 @@
 using Blazored.LocalStorage;
-using LeekLog.Authentication;
 using LeekLog.Data;
 using LeekLog.Data.Extensions;
-using Microsoft.AspNetCore.Components.Authorization;
+using LeekLog.Extensions;
 using Microsoft.EntityFrameworkCore;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -20,15 +19,12 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddSingleton<WeatherForecastService>();
 
-builder.Services.AddDbServices(o =>
-{
-    o.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-});
-
-builder.Services.AddScoped<IUserAuthService, UserAuthService>();
-builder.Services.AddScoped<LeekLogAuthenticationStateProvider>();
-builder.Services.AddScoped<ILeekLogAuthenticationStateProvider>(s => s.GetRequiredService<LeekLogAuthenticationStateProvider>());
-builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<LeekLogAuthenticationStateProvider>());
+builder.Services
+    .AddDbServices(o =>
+    {
+        o.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+    })
+    .AddApiServices();
 
 WebApplication app = builder.Build();
 
